f6a5e3da5c48ce5dd90d8c3bd4a18afe8b0a7dbd
Plywacz <m.plywacz@interia.pl>
2020-03-06 17:15:50 +0100

    Initial commit

---
+++ src/main/java/plywacz/openx/OpenxApplication.java
@@ -0,0 +1,13 @@
+package plywacz.openx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/plywacz/openx/OpenxApplicationTests.java
@@ -0,0 +1,13 @@
+package plywacz.openx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
